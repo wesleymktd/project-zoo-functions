0ada625a961b1674d1719fb3315158adff31fe1a
wesleymktd <wesleymktd@gmail.com>
2022-09-29 20:21:49 -0300

    terminei 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const result = data.species.filter((element) => ids.some((elem) => elem === element.id)); 
+  return result;
 }
 
 module.exports = getSpeciesByIds;
