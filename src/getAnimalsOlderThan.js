a850241bb87f6e9574cdcd9d36f4c2eeb38d9f36
wesleymktd <wesleymktd@gmail.com>
2022-09-29 21:10:00 -0300

    corrigido lint1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = 0;
+  return animals;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const result = data.species.filter((element) => ids.some((elem) => elem === element.id)); 
+  const result = data.species.filter((element) => ids.some((elem) => elem === element.id));
   return result;
 }
 
