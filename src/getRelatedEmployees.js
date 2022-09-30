2c4c9a6d94bede28bcb1e16aeb1a0c58c103a450
wesleymktd <wesleymktd@gmail.com>
2022-09-30 20:05:59 -0300

    terminei 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  if (id === '9e7d4524-363c-416a-8759-8aa7e50c0992' || id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
+  || id === '0e7b460e-acf4-4e17-bcb3-ee472265db83') {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const manager = isManager(managerId);
+  if (!manager) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  return data.employees.filter(({ managers }) => managers.includes(managerId))
+    .map((element) => `${element.firstName} ${element.lastName}`);
 }
+// console.log(getRelatedEmployees(stephanieId))
 
 module.exports = { isManager, getRelatedEmployees };
