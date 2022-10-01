cef5bc4b2342bdaffb2e50b4d0430f04e041713a
wesleymktd <wesleymktd@gmail.com>
2022-10-01 13:52:03 -0300

    10corlint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,9 +1,5 @@
 const data = require('../data/zoo_data');
 
-const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-
 function getOldestFromFirstSpecies(id) {
   const firstIdAnimal = data.employees.filter((element) => element.id === id)[0]
     .responsibleFor[0]; // testado já tenho o primeiro id dos animais que a apessoa colaboradora é responsável
@@ -17,5 +13,5 @@ function getOldestFromFirstSpecies(id) {
 
   return Object.values(animals);
 }
-console.log(getOldestFromFirstSpecies(stephanieId))
+
 module.exports = getOldestFromFirstSpecies;
