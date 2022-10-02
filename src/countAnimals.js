6a1e6040f2a40a352a3eb70d510fed24efed0879
wesleymktd <wesleymktd@gmail.com>
2022-10-01 23:03:12 -0300

    terminei a 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (typeof animal === 'undefined') {
+    const bicho = {};
+    data.species.forEach((element) => {
+      bicho[element.name] = element.residents.length;
+    });
+    return bicho;
+  }
+
+  if (typeof animal.sex === 'string') {
+    return data.species.filter((elen) => elen.name === animal.specie)[0].residents
+      .filter((ele3) => ele3.sex === animal.sex).length;
+  }
+
+  return data.species.filter((elen) => elen.name === animal.specie)[0].residents.length;
 }
 
 module.exports = countAnimals;
